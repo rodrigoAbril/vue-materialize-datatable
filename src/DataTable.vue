56bcc3467018c46e972526b47a7e5426b5bbe281
Rodrigo Abril <rodAbril@iMac.local>
2017-12-05 12:46:09 -0600

    testing

--- src/DataTable.vue
+++ src/DataTable.vue
@@ -104,244 +104,264 @@
 </template>
 
 <script>
-	import Fuse from 'fuse.js';
+import Fuse from "fuse.js";
 
-	export default {
-		props: {
-			title: '',
-			columns: {},
-			rows: {},
-			clickable: {default: true},
-			customButtons: {default: () => []},
-			perPage: {default: () => [10, 20, 30, 40, 50]},
-			defaultPerPage: {default: null},
-			sortable: {default: true},
-			searchable: {default: true},
-			exactSearch: {
-				type: Boolean,
-				default: false
-			},
-			paginate: {default: true},
-			exportable: {default: true},
-			printable: {default: true},
-		},
+export default {
+  props: {
+    title: "",
+    columns: {},
+    rows: {},
+    clickable: { default: true },
+    customButtons: { default: () => [] },
+    perPage: { default: () => [10, 20, 30, 40, 50] },
+    defaultPerPage: { default: null },
+    sortable: { default: true },
+    searchable: { default: true },
+    exactSearch: {
+      type: Boolean,
+      default: false
+    },
+    paginate: { default: true },
+    exportable: { default: true },
+    printable: { default: true }
+  },
 
-		data: () => ({
-			currentPage: 1,
-			currentPerPage: 10,
-			sortColumn: -1,
-			sortType: 'asc',
-			searching: false,
-			searchInput: '',
-		}),
+  data: () => ({
+    currentPage: 1,
+    currentPerPage: 10,
+    sortColumn: -1,
+    sortType: "asc",
+    searching: false,
+    searchInput: ""
+  }),
 
-		methods: {
-			nextPage: function() {
-				if (this.processedRows.length > this.currentPerPage * this.currentPage)
-					++this.currentPage;
-			},
+  methods: {
+    nextPage: function() {
+      if (this.processedRows.length > this.currentPerPage * this.currentPage)
+        ++this.currentPage;
+    },
 
-			previousPage: function() {
-				if (this.currentPage > 1)
-					--this.currentPage;
-			},
+    previousPage: function() {
+      if (this.currentPage > 1) --this.currentPage;
+    },
 
-			onTableLength: function(e) {
-				this.currentPerPage = e.target.value;
-			},
+    onTableLength: function(e) {
+      this.currentPerPage = e.target.value;
+    },
 
-			sort: function(index) {
-				if (!this.sortable)
-					return;
-				if (this.sortColumn === index) {
-					this.sortType = this.sortType === 'asc' ? 'desc' : 'asc';
-				} else {
-					this.sortType = 'asc';
-					this.sortColumn = index;
-				}
-			},
+    sort: function(index) {
+      if (!this.sortable) return;
+      if (this.sortColumn === index) {
+        this.sortType = this.sortType === "asc" ? "desc" : "asc";
+      } else {
+        this.sortType = "asc";
+        this.sortColumn = index;
+      }
+    },
 
-			search: function(e) {
-				this.searching = !this.searching;
-			},
+    search: function(e) {
+      this.searching = !this.searching;
+    },
 
-			click: function(row) {
-				if(!this.clickable){
-					return
-				}
+    click: function(row) {
+      if (!this.clickable) {
+        return;
+      }
 
-				if(getSelection().toString()){
-					// Return if some text is selected instead of firing the row-click event.
-					return
-				}
+      if (getSelection().toString()) {
+        // Return if some text is selected instead of firing the row-click event.
+        return;
+      }
 
-				this.$emit('row-click', row)
-			},
+      this.$emit("row-click", row);
+    },
 
-			exportExcel: function() {
-				const mimeType = 'data:application/vnd.ms-excel';
-				const html = this.renderTable().replace(/ /g, '%20');
+    exportExcel: function() {
+      const mimeType = "data:application/vnd.ms-excel";
+      const html = this.renderTable().replace(/ /g, "%20");
 
-				const documentPrefix = this.title != '' ? this.title.replace(/ /g, '-') : 'Sheet'
-				const d = new Date();
+      const documentPrefix =
+        this.title != "" ? this.title.replace(/ /g, "-") : "Sheet";
+      const d = new Date();
 
-				var dummy = document.createElement('a');
-				dummy.href = mimeType + ', ' + html;
-				dummy.download = documentPrefix
-					+ '-' + d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
-					+ '-' + d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds()
-					+'.xls';
-				dummy.click();
-			},
+      var dummy = document.createElement("a");
+      dummy.href = mimeType + ", " + html;
+      dummy.download =
+        documentPrefix +
+        "-" +
+        d.getFullYear() +
+        "-" +
+        (d.getMonth() + 1) +
+        "-" +
+        d.getDate() +
+        "-" +
+        d.getHours() +
+        "-" +
+        d.getMinutes() +
+        "-" +
+        d.getSeconds() +
+        ".xls";
+      dummy.click();
+    },
 
-			print: function() {
-				let win = window.open("");
-				win.document.write(this.renderTable());
-				win.print();
-				win.close();
-			},
+    print: function() {
+      let win = window.open("");
+      win.document.write(this.renderTable());
+      win.print();
+      win.close();
+    },
 
-			renderTable: function() {
-				var table = '<table><thead>';
+    renderTable: function() {
+      var table = "<table><thead>";
 
-				table += '<tr>';
-				for (var i = 0; i < this.columns.length; i++) {
-					const column = this.columns[i];
-					table += '<th>';
-					table += 	column.label;
-					table += '</th>';
-				}
-				table += '</tr>';
+      table += "<tr>";
+      for (var i = 0; i < this.columns.length; i++) {
+        const column = this.columns[i];
+        table += "<th>";
+        table += column.label;
+        table += "</th>";
+      }
+      table += "</tr>";
 
-				table += '</thead><tbody>';
+      table += "</thead><tbody>";
 
-				for (var i = 0; i < this.rows.length; i++) {
-					const row = this.rows[i];
-					table += '<tr>';
-					for (var j = 0; j < this.columns.length; j++) {
-						const column = this.columns[j];
-						table += '<td>';
-						table +=	this.collect(row, column.field);
-						table += '</td>';
-					}
-					table += '</tr>';
-				}
+      for (var i = 0; i < this.rows.length; i++) {
+        console.group("%cROWS ", "background:#DAF7A6;");
+        const row = this.rows[i];
+        table += "<tr>";
+        for (var j = 0; j < this.columns.length; j++) {
+          console.log(this.collect(row, column.field));
+          const column = this.columns[j];
+          table += "<td>";
+          table += this.collect(row, column.field);
+          table += "</td>";
+        }
+        table += "</tr>";
+      }
 
-				table += '</tbody></table>';
+      table += "</tbody></table>";
 
-				return table;
-			},
+      return table;
+    },
 
-			dig: function(obj, selector) {
-				var result = obj;
-				const splitter = selector.split('.');
+    dig: function(obj, selector) {
+      var result = obj;
+      const splitter = selector.split(".");
 
-				for (let i = 0; i < splitter.length; i++){
-					if (result == undefined)
-						return undefined;
+      for (let i = 0; i < splitter.length; i++) {
+        if (result == undefined) return undefined;
 
-					result = result[splitter[i]];
-				}
+        result = result[splitter[i]];
+      }
 
-				return result;
-			},
+      return result;
+    },
 
-			collect: function(obj, field) {
-				if (typeof(field) === 'function')
-					return field(obj);
-				else if (typeof(field) === 'string')
-					return this.dig(obj, field);
-				else
-					return undefined;
-			}
-		},
+    collect: function(obj, field) {
+      if (typeof field === "function") return field(obj);
+      else if (typeof field === "string") return this.dig(obj, field);
+      else return undefined;
+    }
+  },
 
-		computed: {
-			perPageOptions: function() {
-				var options = (Array.isArray(this.perPage) && this.perPage) || [10, 20, 30, 40, 50];
+  computed: {
+    perPageOptions: function() {
+      var options = (Array.isArray(this.perPage) && this.perPage) || [
+        10,
+        20,
+        30,
+        40,
+        50
+      ];
 
-				// Force numbers
-				options = options.map( v => parseInt(v));
+      // Force numbers
+      options = options.map(v => parseInt(v));
 
-				// Set current page to first value
-				this.currentPerPage = options[0];
+      // Set current page to first value
+      this.currentPerPage = options[0];
 
-				// Sort options
-				options.sort((a,b) => a - b);
+      // Sort options
+      options.sort((a, b) => a - b);
 
-				// And add "All"
-				options.push(-1);
+      // And add "All"
+      options.push(-1);
 
-				// If defaultPerPage is provided and it's a valid option, set as current per page
-				if (options.indexOf(this.defaultPerPage) > -1) {
-					this.currentPerPage = parseInt(this.defaultPerPage);
-				}
+      // If defaultPerPage is provided and it's a valid option, set as current per page
+      if (options.indexOf(this.defaultPerPage) > -1) {
+        this.currentPerPage = parseInt(this.defaultPerPage);
+      }
 
-				return options;
-			},
-			processedRows: function() {
-				var computedRows = this.rows;
+      return options;
+    },
+    processedRows: function() {
+      var computedRows = this.rows;
 
-				if (this.sortable !== false)
-					computedRows = computedRows.sort((x,y) => {
-						if (!this.columns[this.sortColumn])
-							return 0;
+      if (this.sortable !== false)
+        computedRows = computedRows.sort((x, y) => {
+          if (!this.columns[this.sortColumn]) return 0;
 
-						const cook = (x) => {
-							x = this.collect(x, this.columns[this.sortColumn].field);
-							if (typeof(x) === 'string') {
-								x = x.toLowerCase();
-							 	if (this.columns[this.sortColumn].numeric)
-									x = x.indexOf('.') >= 0 ? parseFloat(x) : parseInt(x);
-							}
-							return x;
-						}
+          const cook = x => {
+            x = this.collect(x, this.columns[this.sortColumn].field);
+            if (typeof x === "string") {
+              x = x.toLowerCase();
+              if (this.columns[this.sortColumn].numeric)
+                x = x.indexOf(".") >= 0 ? parseFloat(x) : parseInt(x);
+            }
+            return x;
+          };
 
-						x = cook(x);
-						y = cook(y);
+          x = cook(x);
+          y = cook(y);
 
-						return (x < y ? -1 : (x > y ? 1 : 0)) * (this.sortType === 'desc' ? -1 : 1);
-					})
+          return (
+            (x < y ? -1 : x > y ? 1 : 0) * (this.sortType === "desc" ? -1 : 1)
+          );
+        });
 
-				if (this.searching && this.searchInput) {
-					const searchConfig = { keys: this.columns.map(c => c.field) }
+      if (this.searching && this.searchInput) {
+        const searchConfig = { keys: this.columns.map(c => c.field) };
 
-					// Enable searching of numbers (non-string)
-					// Temporary fix of https://github.com/krisk/Fuse/issues/144
-				    	searchConfig.getFn = function (obj, path) {
-						if(Number.isInteger(obj[path]))
-						return JSON.stringify(obj[path]);
-					    	return obj[path];
-					}
+        // Enable searching of numbers (non-string)
+        // Temporary fix of https://github.com/krisk/Fuse/issues/144
+        searchConfig.getFn = function(obj, path) {
+          if (Number.isInteger(obj[path])) return JSON.stringify(obj[path]);
+          return obj[path];
+        };
 
-					if(this.exactSearch){
-						//return only exact matches
-						searchConfig.threshold = 0,
-						searchConfig.distance = 0
-					}
+        if (this.exactSearch) {
+          //return only exact matches
+          (searchConfig.threshold = 0), (searchConfig.distance = 0);
+        }
 
-					computedRows = (new Fuse(computedRows, searchConfig)).search(this.searchInput);
-				}
+        computedRows = new Fuse(computedRows, searchConfig).search(
+          this.searchInput
+        );
+      }
 
-                return computedRows;
-			},
+      return computedRows;
+    },
 
-			paginated: function() {
-			    var paginatedRows = this.processedRows;
-                if (this.paginate)
-                    paginatedRows = paginatedRows.slice((this.currentPage - 1) * this.currentPerPage, this.currentPerPage === -1 ? paginatedRows.length + 1 : this.currentPage * this.currentPerPage);
-                return paginatedRows;
-            },
+    paginated: function() {
+      var paginatedRows = this.processedRows;
+      if (this.paginate)
+        paginatedRows = paginatedRows.slice(
+          (this.currentPage - 1) * this.currentPerPage,
+          this.currentPerPage === -1
+            ? paginatedRows.length + 1
+            : this.currentPage * this.currentPerPage
+        );
+      return paginatedRows;
+    },
 
-			computedClass: function() {
-				return this.class = 'responsive-table'
-			}
-		},
+    computedClass: function() {
+      return (this.class = "responsive-table");
+    }
+  },
 
-		mounted: function() {
-			this.currentPerPage = this.currentPerPage
-		}
-	}
+  mounted: function() {
+    this.currentPerPage = this.currentPerPage;
+  }
+};
 </script>
 <style scoped src="materialize-css/dist/css/materialize.min.css"></style>
 
