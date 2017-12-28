226282a1c69244666be24a7a2cdf3e0969206987
Rodrigo Abril <rodAbril@iMac.local>
2017-12-28 02:57:22 -0600

    add td custom class

--- src/DataTable.vue
+++ src/DataTable.vue
@@ -61,7 +61,7 @@
 
 			<tbody>
 				<tr v-for="(row, index) in paginated" :class="{ clickable : clickable }" @click="click(row)">
-					<td v-for="column in columns" :class=" {numeric : column.numeric }, {'iconValue'column.field : column.iconValue } ">
+					<td v-for="column in columns" :class=" {numeric : column.numeric }, {iconValue : column.iconValue} ">
 						<div v-if="!column.html"> {{ collect(row, column.field) }} </div>
 						<div v-if="column.html" v-html="collect(row, column.field)"></div>
 					</td>
